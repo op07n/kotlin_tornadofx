849f42c0e28c5d0ca726d599ebdbab909eb62214
op07n <39182105+op07n@users.noreply.github.com>
2020-05-01 18:45:58 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,95 +1,76 @@
-import org.gradle.api.JavaVersion.VERSION_11
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
-	kotlin("jvm") version "1.3.0"
-	id("application")
-	id("org.openjfx.javafxplugin") version "0.0.7"
-//	id("com.github.johnrengelman.shadow") version "5.1.0"
+  kotlin("jvm") version "1.3.71"
+  id("java")
+  id("com.github.johnrengelman.shadow") version "5.2.0"
+  id("application")
+  id("org.openjfx.javafxplugin") version "0.0.8"
 }
 
-// val applicationVersion: String by project
+group = "com.pokosho.midi2musicxml"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
 
-group = "de.klg71.tornado"
-version = "1.0"
+val javafxVersion = "11.0.2"
 
-java {
-	sourceCompatibility = VERSION_11
-	targetCompatibility = VERSION_11
+repositories {
+  mavenCentral()
 }
-
-
 
 dependencies {
-    compile(kotlin("stdlib-jdk8"))
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+  implementation("org.thymeleaf:thymeleaf:3.0.11.RELEASE")
+  implementation("org.slf4j:slf4j-log4j12:1.7.30")
+  implementation("com.google.guava:guava:29.0-jre")
+  implementation("com.atilika.kuromoji:kuromoji-ipadic:0.9.0")
+  implementation("org.openjfx:javafx-controls:14")
+  implementation("no.tornado:tornadofx:1.7.17")
+  compileOnly("org.openjfx:javafx-graphics:$javafxVersion.version:win")
+  compileOnly("org.openjfx:javafx-graphics:$javafxVersion:linux")
+  compileOnly("org.openjfx:javafx-graphics:$javafxVersion:mac")
 
-    // Add the tornadofx dependency
-    compile("no.tornado:tornadofx:1.7.17")
-}
-repositories {
-    mavenCentral()
-}
-
-javafx {
-
-    // Declare the javafx modules we need to use
-    modules("javafx.controls")
-}
-
-//application {
-
-    // Declare the main class for the application plugin
-//    mainClassName = "de.klg71.tornado.MainKt"
-//}
-
- 
-//tasks.withType(Jar::class) {
-//    manifest {
-//        attributes["Manifest-Version"] = "1.0"
-//        attributes["Main-Class"] = "de.klg71.tornado.MainKt"
-//        attributes["Author"] = "sdfaffdasf"
-//    }
-//}
-
-
-
-tasks.withType<Jar> {
-    // Otherwise you'll get a "No main manifest attribute" error
-    manifest {
-        attributes["Main-Class"] = "de.klg71.tornado.MainKt"
-    }
-
-    // To add all of the dependencies otherwise a "NoClassDefFoundError" error
-    from(sourceSets.main.get().output)
-
-    dependsOn(configurations.runtimeClasspath)
-    from({
-        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
-    })
+  testImplementation("org.mockito:mockito-junit-jupiter:3.3.3")
+  testImplementation("org.junit.jupiter:junit-jupiter-engine:5.6.2")
 }
 
-
-
-
-
-//tasks.jar {
-//tasks.withType(Jar::class) {	
-//	manifest {
-//		attributes["Main-Class"] = "${project.group}.MainKt"
-//	}
-//}
-
-application {
-    mainClassName = "${project.group}.MainKt"
+tasks.withType<Test> {
+  useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions.jvmTarget = "1.8"
+  kotlinOptions {
+    freeCompilerArgs = listOf(
+      "-Xjsr305=strict",
+      "-Xopt-in=kotlin.ExperimentalUnsignedTypes"
+    )
+    jvmTarget = "11"
+  }
 }
 
+tasks {
+  named<ShadowJar>("shadowJar") {
+    archiveBaseName.set("kotlin_tornadofx")
+    mergeServiceFiles()
+    manifest {
+      attributes(mapOf("Main-Class" to "com.github.csolem.gradle.shadow.kotlin.example.App"))
+    }
+  }
+}
 
+tasks {
+  build {
+    dependsOn(shadowJar)
+  }
+}
 
-// Set the jvmTarget to 1.8 to support inlining
-// tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> { kotlinOptions.jvmTarget = "1.8" }
+javafx {
+  version = javafxVersion
+  modules = listOf("javafx.controls", "javafx.graphics", "javafx.fxml")
+}
 
+application {
+  mainClassName = "de.klg71.tornado.MainKt"
+}
